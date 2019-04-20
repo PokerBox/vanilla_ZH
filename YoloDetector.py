e9dc2d42af1cc01e3b7c63a44d7d4d96b1890e10
Zhenghao Zhao <zhao.1980@osu.edu>
2019-04-20 15:29:37 -0400

    calibrate original x

--- YoloDetector.py
+++ YoloDetector.py
@@ -302,10 +302,10 @@ def StartYoloDetection(cap, tracker, cvth, serial, debug=False, thresh=0.10):
                 # y = math.atan((pix_y - 416/2) / 693.3) * 1800 / math.pi
 
                 # New calibration
-                x = math.atan((pix_x - 740/2) / 475.3) * 1800 / math.pi
-                print('calx:',pix_x)
+                x = math.atan((pix_x*xratio - 740/2) / 475.3) * 1800 / math.pi
+                print('calx:', pix_x)
                 y = math.atan((pix_y - 416/2) / 475.3) * 1800 / math.pi
-                print('caly:',pix_y)
+                print('caly:', pix_y)
 
                 # serial.filter.updateSensorAbsolute(x, 0, yaw, 0)
 
